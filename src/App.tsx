26791e9115ef2f782d6f1e8999628d2c1f69ff89
Yon Refaeli <>
2025-05-28 14:18:24 -0400

    feat(medal-count): fetch live data
    - Remove hardcoded medal-data.ts
    - Set up a tiny local express server and vite proxy
    - Serve either the mock medals.json or a 500 based on the "--error" arg
    - use react-error-boundary for a fallback in case of error

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { medalData } from "./features/medals/medal-data";
+import { useEffect, useState } from "react";
 import { MedalsPage } from "./pages/MedalsPage";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
+  const [medals, setMedals] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/medals")
+      .then((data) => data.json())
+      .then((data) => setMedals(data));
+  }, []);
+
   return (
     <>
-      <MedalsPage medals={medalData} />
+      <ErrorBoundary
+        fallback={
+          <div className="text-lg mt-32 mx-auto max-w-md text-center">
+            Error: We couldn't find your stuff 😢
+          </div>
+        }
+      >
+        <MedalsPage medals={medals} />
+      </ErrorBoundary>
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    },
+  },
 })
